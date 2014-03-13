d76feaac3e6a407c00c228ace0851051fb7893d5
Brad Zeis <bradzeis@gmail.com>
2014-03-12 21:58:38 -0500

    Initial commit.

---
+++ Neon/Neon/NNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NNAppDelegate.h
+//  Neon
+//
+//  Created by Brad Zeis on 3/12/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Neon/Neon/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Neon
+//
+//  Created by Brad Zeis on 3/12/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NNAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NNAppDelegate class]));
+    }
+}
